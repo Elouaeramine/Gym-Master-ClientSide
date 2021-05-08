823cb20cae6deb4f80e4355f84ca808fd8718697
Sarah Benabdallah <71703426+SarahBenabdallah@users.noreply.github.com>
2021-05-08 22:44:22 +0100

    final responsive navbar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 // import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,14 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent{
+
   
 log(x: any) { console.log(x); }
 submitted = false;
 
 onSubmit() { this.submitted = true; }
-preview(e: { preventDefault: () => void; }) {
-  e.preventDefault();
-  console.log('preview')
+
+
+register (loginForm: NgForm){
+  console.log('Successful registration');
+  console.log(loginForm);
 }
+
+// preview(e: { preventDefault: () => void; }) {
+//   e.preventDefault();
+//   console.log('preview')
+// }
 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class NavbarComponent {
+  showMenu = false;
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
   }
-
+ 
 }
