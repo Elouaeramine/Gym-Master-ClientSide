20fe8aa5337d06d717cf28ce29d9a2d095714906
Sarah Benabdallah <71703426+SarahBenabdallah@users.noreply.github.com>
2021-05-13 04:12:02 +0100

    create loginpage
    name of the login component is login2 !!!!!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { DicoverGymsHeaderComponent } from './shared/header/dicover-gyms-header/
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { GymCardComponent } from './DiscGyms/gym-card/gym-card.component';
 import { GymCardsListComponent } from './DiscGyms/gym-cards-list/gym-cards-list.component';
+import { Login2Component } from './login2/login2.component';
+import { Login2PageComponent } from './pages/login2-page/login2-page.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { GymCardsListComponent } from './DiscGyms/gym-cards-list/gym-cards-list.
     DiscoverGymsPageComponent,
     DicoverGymsHeaderComponent,
     HomePageComponent,
-    GymCardsListComponent
+    GymCardsListComponent,
+    Login2Component,
+    Login2PageComponent
     
   ],
   imports: [
---
+++ src/app/pages/login2-page/login2-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login2-page',
+  templateUrl: './login2-page.component.html',
+  styleUrls: ['./login2-page.component.scss']
+})
+export class Login2PageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
